d1ee2b48203cd5485b16358f676522f3ff51669f
ntbd <72973160+ntbd@users.noreply.github.com>
2020-10-16 14:17:27 +0100

    Update DAPWebScripts.js

--- DAPWebScripts.js
+++ DAPWebScripts.js
@@ -1,1 +1,13 @@
-alert("bruh");
+var settings = {
+  "url": "https://sheets.googleapis.com/v4/spreadsheets/1gy11q_35MO6sjUhifpXdVah-PeQx0p390iMHF9GNiDs/values/Destination!B2:B13?key=AIzaSyAd7R-JfZ3mMBVPELIw0FhGdetdJBhMXiA",
+  "method": "GET",
+  "dataType" : 'JSON',
+  "timeout": 0,
+};
+
+function runAPI() {
+  $.ajax(settings).done(function (response) {
+    var obj = JSON.stringify(response);
+    alert(obj);
+  });
+}
